ffd54c1b3a26f980823d9465d33ff9dee52534a6
Jinwoo Park <zvzv9808@gmail.com>
2023-04-11 00:04:15 +0900

    test middleware

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,33 @@
 "use strict";
-console.log("hello world!!");
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app_model_1 = require("./app.model");
+var app = express();
+var port = 8000;
+app.use(function (req, res, next) {
+    console.log("this is logging middleware");
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log("this is som middleware");
+    next();
+});
+app.get("/", function (req, res) {
+    res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    console.log(req.rawHeaders[1]);
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found errer" });
+});
+app.listen(port, function () {
+    console.log("Example app listening at http://localhost:" + port);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,39 @@
 import * as express from "express";
-import { Cat, CatType } from "app.model";
+import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 const port = 8000;
 
-const data = [1, 2, 3, 4];
+app.use((req, res, next) => {
+  console.log("this is logging middleware");
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  console.log("this is som middleware");
+  next();
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get(
+  "/cats/blue",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    res.send({ blue: Cat[0] });
+  }
+);
+
+app.get("/cats/som", (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found errer" });
 });
 
 app.listen(port, () => {
