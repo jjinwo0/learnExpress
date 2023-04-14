b3a5abf32e945716a83cb1c02e3b6f7a7b99871d
Jinwoo Park <zvzv9808@gmail.com>
2023-04-14 23:29:20 +0900

    Create Cat API

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,54 @@ app.use((req, res, next) => {
   next();
 });
 
+//* json middleware
+app.use(express.json());
+
 //* READ 전체 고양이 데이터 조회
 app.get("/cats", (req, res) => {
   try {
     const cats = Cat;
-    res.send({
+    res.status(200).send({
       success: true,
       data: {
         cats,
       },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: console.error(),
+    });
+  }
+});
+
+//* READ 특정 고양이 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const cats = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: console.error(),
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가 API
+app.post("/cats", (req, res) => {
+  try {
+    res.status(200).send({
+      success: true,
+      data: {},
     });
   } catch (error) {
     res.status(400).send({
